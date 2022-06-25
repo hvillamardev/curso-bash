ed9fd86248591cd953fae1f247d13c91c21f32d1
Hilario Villamar <hilario_villamar@hotmail.com>
2022-06-25 12:21:37 -0500

    Ejercicio 7

--- 7_read.sh
+++ 7_read.sh
@@ -1,15 +1,11 @@
 # ! /bin/bash
-#Programa para ejemplificar como capturar la informacion del usuario utilizando el comando echo, read y $REPLY
+#Programa para ejemplificar como capturar la informacion del usuario utilizando el comando read
 #Autor: Hilario Villamar
 
 option=0
 backupName=''
 
 echo 'Programa utilidades Postgres'
-echo -n 'Ingresar un opcion:'
-read
-option=$REPLY
-echo -n 'Ingresar el nombre del archivo del backup:'
-read
-backupName=$REPLY
+read -p 'Ingresar un opcion:' option
+read -p 'Ingresar el nombre del archivo del backup:' backupName
 echo "Opcion:$option. backupNmae:$backupName"
